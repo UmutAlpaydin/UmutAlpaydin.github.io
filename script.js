973866ab90c665c0756ff3252c9a47f96cd1d3fc
Umut Alpaydın <56254593+UmutAlpaydin@users.noreply.github.com>
2022-02-02 15:39:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing-2", {
-        strings: ["Software Enginerer", "Python Developer", "AI Engineer" ],
+        strings: ["Software Engineer", "Python Developer", "AI Engineer" ],
         typeSpeed: 85,
         backSpeed: 60,
         loop: true
